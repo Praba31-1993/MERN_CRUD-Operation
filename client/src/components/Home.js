c4576b625bfcad9f714b878179e7c91ed457f3b1
Prabakaran <prabakaran.dhanasekar@twilightsoftwares.com>
2022-12-24 19:05:35 +0530

    table design

---
+++ client/src/components/Home.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="mt-5">
+      <div className="container">
+        <div className="add_btn mt-2">
+          <button className="btn btn-primary">Add Data</button>
+        </div>
+      </div>
+      {/* Table */}
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Home;
